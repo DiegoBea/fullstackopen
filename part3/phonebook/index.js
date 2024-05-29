682fdc84453125386db7b3f7f37f243c0c52ec5c
Diego Bea <diegobeagomez1@gmail.com>
2024-05-29 21:24:16 +0200

    PhoneBook - Test

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -110,12 +110,6 @@ app.get("/info", (request, response) => {
   response.send(info);
 });
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
-
-app.use(unknownEndpoint);
-
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);

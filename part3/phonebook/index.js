8ab9c966c4c45f1bb3c522e72913a3d2a046601b
Diego Bea <diegobeagomez1@gmail.com>
2024-06-12 19:53:04 +0200

    Phonebook - Middleware - Update and delete persons

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -66,18 +66,11 @@ app.get("/api/persons/:id", (request, response) => {
   )
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  let id = Number(request.params.id);
-  let person = persons.find((person) => person.id === id);
-
-  if (!person) {
-    return response.status(404).json({
-      error: "Person not found",
-    });
-  }
-
-  persons = persons.filter((person) => person.id !== id);
-  response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end()
+    }).catch(error => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
@@ -86,29 +79,78 @@ app.post("/api/persons", (request, response) => {
     response.status(500).json({ error: "Incomplete parameters" });
   }
 
-  const person = new Person({
-    name: body.name,
-    number: body.number
-  });
+  Person.findOne({ 'name': body.name })
+    .then(person => {
+      if (!person) {
+        person = new Person()
+      }
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
-  morgan.token("dev", function (req, res) {
-    return req.headers["content-type"];
-  });
+      person.name = body.name
+      person.number = body.number
+
+      person.save().then(savedPerson => {
+        response.json(savedPerson)
+      })
+
+      morgan.token("dev", function (req, res) {
+        return req.headers["content-type"];
+      });
+    }).catch(error => next(error))
+
+  // const person = new Person({
+  //   name: body.name,
+  //   number: body.number
+  // });
+
+  // person.save().then(savedPerson => {
+  //   response.json(savedPerson)
+  // })
+
+  // morgan.token("dev", function (req, res) {
+  //   return req.headers["content-type"];
+  // });
 });
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    }).catch(error => next(error))
+})
+
 app.get("/info", (request, response) => {
-  let info =
-    "<div>Phonebook has info for " +
-    persons.length +
-    " people<br/>" +
-    new Date().toLocaleString() +
-    "</div>";
-  response.send(info);
+  Person.find({}).then(persons => {
+    let info =
+      "<div>Phonebook has info for " +
+      persons.length +
+      " people<br/>" +
+      new Date().toLocaleString() +
+      "</div>";
+    response.send(info);
+  });
 });
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "Unknown endpoint" });
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') return response.status(400).send({ error: 'malformatted id' })
+
+  next(error)
+}

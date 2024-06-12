34acca61b982d75fb36266e443480d7c652f2c33
Diego Bea <diegobeagomez1@gmail.com>
2024-06-12 19:39:09 +0200

    Notes - Middleware - Update and delete notes

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -37,18 +37,24 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -68,24 +74,41 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-app.put('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-
-  updatedNote = notes.find((note) => note.id === id);
+app.put('/api/notes/:id', (request, response, next) => {
   const body = request.body
 
-  if (updatedNote) {
-    updatedNote.content = body.content || updatedNote.content;
-    updatedNote.important = Boolean(body.important ?? updatedNote.important);
-    notes = notes.map((note) => {
-      return note.id === id ? updatedNote : note;
-    })
-    response.json(updatedNote);
-  } else {
-    response.status(404).end()
+  const note = {
+    content: body.content,
+    important: body.important,
   }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// controlador de solicitudes con endpoint desconocido
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// este debe ser el último middleware cargado, ¡también todas las rutas deben ser registrada antes que esto!
+app.use(errorHandler)

0498e14e6657e4633c0bfd1f0c4a9373e55b3b3b
Diego Bea <diegobeagomez1@gmail.com>
2025-02-13 19:51:51 +0100

    Blogs - 4.A - Ejercicios 4.1-4.2

---
+++ part4/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const cors = require('cors')
+const config = require('./utils/config')
+const logger = require("./utils/logger")
+const blogsRouter = require("./controllers/blogs")
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app;
